f955d1a514a4745bd6a0f309b4084bbb0a4211a9
yuicchicode <Yuicchisama@gmail.com>
2022-08-04 11:40:50 -0300

    Files Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let seuVotoPara = document.querySelector('.s-1-1 span');
+let cargo = document.querySelector('.s-1-2 span');
+let descricao = document.querySelector('.s-1-4');
+let aviso = document.querySelector('.s-2');
+let lateral = document.querySelector('.s-1-right');
+let numeros = document.querySelector('.s-1-3');
---
+++ way.js
@@ -0,0 +1,50 @@
+let way = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Joanna Da Arc',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'candidato.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Bread o Pitt',
+                partido: 'DEFG',
+                fotos: [
+                    {url: 'candidato.jpg',legenda: 'Vereador',small: false}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Pris Catt',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url: 'candidato.jpg', legenda: 'Prefeito', small: false},
+                    {url: 'candidato.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Com Truise',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url: 'candidato.jpg', legenda: 'Prefeito', small: false},
+                    {url: 'candidato.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+];
